1a3a0c5b19ddc3f5d619784e7af0d8fe9da56ad8
Lenz Baumann <holzmano@Lenzs-MBP.fritz.box>
2022-11-01 21:12:17 +0100

    redirecting urls basic to ''

--- paper_butler/paper_butler/urls.py
+++ paper_butler/paper_butler/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings  # add this
 from django.conf.urls.static import static  #
 
 urlpatterns = [
-                  path('basic/', include('basic.urls')),
+                  path('', include('basic.urls')),
                   path('admin/', admin.site.urls),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
